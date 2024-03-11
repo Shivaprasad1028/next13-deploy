dae0f222b416e62d410ad3fa9d697ea92b2fb497
Shivaprasad1028 <121084091+Shivaprasad1028@users.noreply.github.com>
2024-03-11 16:25:19 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import logo from '../Images/logo.png';
 const Footer = () => {
 return(
 <div className="text-black bg-amazon_light h-20 justify-center text-center items-center flex pt-3">
-  <Image  src={logo} className=' w-28'/>
+  <Image  src={logo} alt="logoImgn"  className=' w-28'/>
   <div className='text-sm  flex items-center justify-center pb-4 ml-2 text-gray-300'>
   <p className=''> All rights are reserved <span className='hover:text-white cursor-pointer hover:underline  duration-300'> <a href=" "> @Shiva.com </a></span></p></div>
 </div>
